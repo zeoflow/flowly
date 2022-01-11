dc58df5af8b23d39c50b24158a68bfd80ccc7e40
Teodor G <grigor.teodor@gmail.com>
2022-01-11 14:57:50 +0200

    created runtime library

---
+++ runtime/src/main/java/com/zeoflow/flowly/LifecycleRegistryOwner.java
@@ -0,0 +1,15 @@
+package com.zeoflow.flowly;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @deprecated Use {@code androidx.appcompat.app.AppCompatActivity}
+ * which extends {@link LifecycleOwner}, so there are no use cases for this class.
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+@Deprecated
+public interface LifecycleRegistryOwner extends LifecycleOwner {
+    @NonNull
+    @Override
+    LifecycleRegistry getLifecycle();
+}
